fccc018b1aa4bacb60aace15b20ed4813c80d71f
xyx <82051300+axin1301@users.noreply.github.com>
2023-11-06 16:20:21 +0200

    Add files via upload

--- compute_statis.py
+++ compute_statis.py
@@ -3,10 +3,9 @@ import pandas as pd
 data = pd.read_csv('validation_statistics_first10_d500_recall.csv')
 # 按照指定列进行分组，计算每组的 pre, rec, f1s 值
 grouped = data.groupby(['county', 'roadclass', 'year', 'i']).apply(lambda x: pd.Series({
-    'pre': (x['pre'] * x['count']).sum() / x['count'].sum(),  # 计算 pre 值
-    'rec': (x['rec'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    'f1s': (x['f1s'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    # 'f1s': (2 * (x['pre'] * x['rec']) / (x['pre'] + x['rec'])).mean()  # 计算 f1s 值
+    'pre': x['pre'].mean(),  # 计算 pre 值
+    'rec': x['rec'].mean(),  # 计算 rec 值
+    'f1s': x['f1s'].mean()  # 计算 f1s 值
 }))
 
 # 重置索引
@@ -20,10 +19,9 @@ grouped.to_csv('validation_statistics_first10_d500_recall_grouped.csv', index=Fa
 data = pd.read_csv('validation_statistics_first10_OSM_recall.csv')
 # 按照指定列进行分组，计算每组的 pre, rec, f1s 值
 grouped = data.groupby(['county', 'roadclass', 'year', 'i']).apply(lambda x: pd.Series({
-    'pre': (x['pre'] * x['count']).sum() / x['count'].sum(),  # 计算 pre 值
-    'rec': (x['rec'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    'f1s': (x['f1s'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    # 'f1s': (2 * (x['pre'] * x['rec']) / (x['pre'] + x['rec'])).mean()  # 计算 f1s 值
+    'pre': x['pre'].mean(),  # 计算 pre 值
+    'rec': x['rec'].mean(),  # 计算 rec 值
+    'f1s': x['f1s'].mean()  # 计算 f1s 值
 }))
 
 # 重置索引
@@ -37,10 +35,9 @@ grouped.to_csv('validation_statistics_first10_OSM_recall_grouped.csv', index=Fal
 data = pd.read_csv('validation_statistics_first10_b1_recall.csv')
 # 按照指定列进行分组，计算每组的 pre, rec, f1s 值
 grouped = data.groupby(['county', 'roadclass', 'year', 'i']).apply(lambda x: pd.Series({
-    'pre': (x['pre'] * x['count']).sum() / x['count'].sum(),  # 计算 pre 值
-    'rec': (x['rec'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    'f1s': (x['f1s'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    # 'f1s': (2 * (x['pre'] * x['rec']) / (x['pre'] + x['rec'])).mean()  # 计算 f1s 值
+    'pre': x['pre'].mean(),  # 计算 pre 值
+    'rec': x['rec'].mean(),  # 计算 rec 值
+    'f1s': x['f1s'].mean()  # 计算 f1s 值
 }))
 
 # 重置索引
@@ -54,10 +51,9 @@ grouped.to_csv('validation_statistics_first10_b1_recall_grouped.csv', index=Fals
 data = pd.read_csv('validation_statistics_first10_b2_recall.csv')
 # 按照指定列进行分组，计算每组的 pre, rec, f1s 值
 grouped = data.groupby(['county', 'roadclass', 'year', 'i']).apply(lambda x: pd.Series({
-    'pre': (x['pre'] * x['count']).sum() / x['count'].sum(),  # 计算 pre 值
-    'rec': (x['rec'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    'f1s': (x['f1s'] * x['count']).sum() / x['count'].sum(),  # 计算 rec 值
-    # 'f1s': (2 * (x['pre'] * x['rec']) / (x['pre'] + x['rec'])).mean()  # 计算 f1s 值
+    'pre': x['pre'].mean(),  # 计算 pre 值
+    'rec': x['rec'].mean(),  # 计算 rec 值
+    'f1s': x['f1s'].mean()  # 计算 f1s 值
 }))
 
 # 重置索引
